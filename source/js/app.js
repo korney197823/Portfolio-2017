5d78e395e8493091113f237342f4b25dad977e99
Denis Kornienko <korney197823@gmail.com>
2017-03-05 22:46:15 +0300

    add styles for about me  mobile version - features block, added google maps and footer

--- source/js/app.js
+++ source/js/app.js
@@ -17,41 +17,141 @@
   
   
 })();
-(function(window, document) {
-  'use strict';
-  let file = 'assets/img/sprite.svg', // путь к файлу спрайта на сервере
-    revision = 1;            // версия спрайта
-  if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect) return true;
-  let isLocalStorage = 'localStorage' in window && window['localStorage'] !== null,
-    request,
-    data,
-    insertIT = function() {
-      document.body.insertAdjacentHTML('afterbegin', data);
-    },
-    insert = function() {
-      if (document.body) insertIT();
-      else document.addEventListener('DOMContentLoaded', insertIT);
-    };
-  if (isLocalStorage && localStorage.getItem('inlineSVGrev') == revision) {
-    data = localStorage.getItem('inlineSVGdata');
-    if (data) {
-      insert();
-      return true;
-    }
-  }
-  try {
-    request = new XMLHttpRequest();
-    request.open('GET', file, true);
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400) {
-        data = request.responseText;
-        insert();
-        if (isLocalStorage) {
-          localStorage.setItem('inlineSVGdata', data);
-          localStorage.setItem('inlineSVGrev', revision);
-        }
+
+var map;
+
+var style = [
+  {
+    "featureType": "administrative",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#444444"
       }
-    }
-    request.send();
-  } catch (e) {}
-}(window, document));
+    ]
+  },
+  {
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [
+      {
+        "color": "#f2f2f2"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [
+      {
+        "saturation": -100
+      },
+      {
+        "lightness": 45
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "simplified"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [
+      {
+        "color": "#61dac9"
+      },
+      {
+        "visibility": "on"
+      }
+    ]
+  }
+];
+
+
+
+
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 44.601255, lng: 38.041145},
+    zoom: 18,
+    panControl: false,
+    zoomControl: false,
+    mapTypeControl: false,
+    streetViewControl: false,
+    scrollwheel: false,
+    styles: style
+  });
+}
+// (function(window, document) {
+//   'use strict';
+//   let file = 'assets/img/sprite.svg', // путь к файлу спрайта на сервере
+//     revision = 1;            // версия спрайта
+//   if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect) return true;
+//   let isLocalStorage = 'localStorage' in window && window['localStorage'] !== null,
+//     request,
+//     data,
+//     insertIT = function() {
+//       document.body.insertAdjacentHTML('afterbegin', data);
+//     },
+//     insert = function() {
+//       if (document.body) insertIT();
+//       else document.addEventListener('DOMContentLoaded', insertIT);
+//     };
+//   if (isLocalStorage && localStorage.getItem('inlineSVGrev') == revision) {
+//     data = localStorage.getItem('inlineSVGdata');
+//     if (data) {
+//       insert();
+//       return true;
+//     }
+//   }
+//   try {
+//     request = new XMLHttpRequest();
+//     request.open('GET', file, true);
+//     request.onload = function() {
+//       if (request.status >= 200 && request.status < 400) {
+//         data = request.responseText;
+//         insert();
+//         if (isLocalStorage) {
+//           localStorage.setItem('inlineSVGdata', data);
+//           localStorage.setItem('inlineSVGrev', revision);
+//         }
+//       }
+//     }
+//     request.send();
+//   } catch (e) {}
+// }(window, document));
